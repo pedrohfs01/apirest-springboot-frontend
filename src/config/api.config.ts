6c31f0f04a6381a3f2c5e478a1afaba10d34339a
Pedro Henrique Fernandes Santos <pedexg@gmail.com>
2020-11-19 12:46:35 -0300

    Mostrando categorias

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,5 @@
 export const API_CONFIG = {
-    baseURL: "http://localhost:8080/"
+    baseURL: "http://localhost:8080/",
+    bucketBaseURL: "https://pedrofernandes-spring-ionic.s3-sa-east-1.amazonaws.com/"
 
 }
