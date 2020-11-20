5e8d2c5946def24547dfabda276170f3e02da293
Pedro Henrique Fernandes Santos <pedexg@gmail.com>
2020-11-20 11:19:30 -0300

    Povoando dinamicamente estado e cidade

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { CidadeDTO } from '../../models/cidade.dto';
+import { EstadoDTO } from '../../models/estado.dto';
+import { CidadeService } from '../../services/domain/cidade.service';
+import { EstadoService } from '../../services/domain/estado.service';
 
 @IonicPage()
 @Component({
@@ -11,9 +15,14 @@ export class SignupPage {
 
   formGroup: FormGroup;
 
+  estados: EstadoDTO[];
+  cidades: CidadeDTO[];
+
   constructor(public navCtrl: NavController, 
     public navParams: NavParams,
-    public formBuilder:FormBuilder) {
+    public formBuilder:FormBuilder,
+    public estadoService: EstadoService,
+    public cidadeService: CidadeService) {
 
       this.formGroup = this.formBuilder.group({
         nome : ['pedro', [Validators.required, Validators.minLength(5), Validators.maxLength(120)]],
@@ -35,7 +44,23 @@ export class SignupPage {
   }
 
   ionViewDidLoad() {
-    
+    this.estadoService.findAll().subscribe(
+      response => {
+        this.estados = response;
+        this.formGroup.controls.estadoId.setValue(this.estados[0].id);
+        this.updateCidades();
+      },
+      error => {
+      }
+    );
+  }
+
+  updateCidades(){
+    let estado_id = this.formGroup.value.estadoId;
+    this.cidadeService.findAll(estado_id).subscribe(response =>{
+      this.cidades = response;
+      this.formGroup.controls.cidadeId.setValue(null);
+    }, error =>{});
   }
   signupUser(){
 
