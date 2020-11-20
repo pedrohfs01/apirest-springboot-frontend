0c5b4aca044b69157d96fa53c371ab3017fb442a
Pedro Henrique Fernandes Santos <pedexg@gmail.com>
2020-11-20 10:54:23 -0300

    Controlando dados do registro

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 @IonicPage()
@@ -8,13 +9,35 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SignupPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  formGroup: FormGroup;
+
+  constructor(public navCtrl: NavController, 
+    public navParams: NavParams,
+    public formBuilder:FormBuilder) {
+
+      this.formGroup = this.formBuilder.group({
+        nome : ['pedro', [Validators.required, Validators.minLength(5), Validators.maxLength(120)]],
+        email: ['pedro@gmail.com', [Validators.required, Validators.email]],
+        tipo: ['1', [Validators.required]],
+        cpfOuCnpj: ['31192252071', [Validators.required, Validators.minLength(11), Validators.maxLength(14)]],
+        senha: ['123', [Validators.required]],
+        logradouro: ['Brasilia', [Validators.required]],
+        numero: ['100', [Validators.required]],
+        complemento: ['Apt 100', []],
+        bairro: ['Sobradinho', []],
+        cep: ['73000000', [Validators.required]],
+        telefone1: ['989292912', [Validators.required]],
+        telefone2: ['', []],
+        telefone3: ['', []],
+        estadoId: [null, [Validators.required]],
+        cidadeId: [null, []]
+      });
   }
 
   ionViewDidLoad() {
     
   }
   signupUser(){
-    
+
   }
 }
