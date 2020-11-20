2a8106a8c38170d4619f2051ac44b1759b1d47b3
Pedro Henrique Fernandes Santos <pedexg@gmail.com>
2020-11-20 13:37:11 -0300

    Adicionando imagem nos produtos

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { API_CONFIG } from '../../config/api.config';
 import { ProdutoDTO } from '../../models/produtos.dto';
 import { ProdutoService } from '../../services/domain/produto.service';
 
@@ -13,15 +14,27 @@ export class ProdutosPage {
   items: ProdutoDTO[];
 
   constructor(public navCtrl: NavController,
-     public navParams: NavParams,
-     public produtoService: ProdutoService) {
+    public navParams: NavParams,
+    public produtoService: ProdutoService) {
   }
 
   ionViewDidLoad() {
     let categoria_id = this.navParams.get("categoria_id");
     this.produtoService.findByCategoria(categoria_id)
-    .subscribe(response =>{
-      this.items = response['content'];
-    }),error=>{};
+      .subscribe(response => {
+        this.items = response['content'];
+        this.loadImageUrls();
+      }), error => { };
   };
+  loadImageUrls() {
+    for(let i=0; i<this.items.length;i++){
+      let item = this.items[i];
+      this.produtoService.getSmallImageFromBucket(item.id)
+      .subscribe(response => {
+        item.imageUrl = `${API_CONFIG.bucketBaseURL}/prod${item.id}-small.jpg`;
+      },
+        error => { });
+    }
+  }
+
 }
