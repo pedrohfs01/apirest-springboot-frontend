655ea74ec6599454f8a44dc68eef31aad70801fc
Pedro Henrique Fernandes Santos <pedexg@gmail.com>
2020-11-25 08:54:07 -0300

    Página de escolha de endereco criada

--- src/pages/cart/cart.ts
+++ src/pages/cart/cart.ts
@@ -56,4 +56,8 @@ export class CartPage {
   goOn(){
     this.navCtrl.setRoot('CategoriasPage');
   }
+
+  checkOut(){
+    this.navCtrl.push("PickAdressPage");
+  }
 }
---
+++ src/pages/pick-address/pick-address.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { EnderecoDTO } from '../../models/endereco.dto';
+
+@IonicPage()
+@Component({
+  selector: 'page-pick-address',
+  templateUrl: 'pick-address.html',
+})
+export class PickAddressPage {
+
+  items: EnderecoDTO[];
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad PickAddressPage');
+  }
+
+}
